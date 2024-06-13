1c51a7f8f0d4e34cc2b8b8485cda220bd73fff12
Lucie Gernidos <lgernido@made-f0Dr10s12.clusters.42paris.fr>
2024-06-13 15:59:46 +0200

    ok

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 11:54:39 by lgernido          #+#    #+#             */
+/*   Updated: 2024/06/13 13:32:49 by lgernido         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <string>
+#include <iostream>
+
+template<typename T>
+
+
+
+#ifndef
