64e3faabc145253862a92c5345c86a70789671fb
Lucie Gernidos <lgernido@made-f0Dr11s14.clusters.42paris.fr>
2024-06-14 15:10:29 +0200

    on avance

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,18 +6,54 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 11:54:39 by lgernido          #+#    #+#             */
-/*   Updated: 2024/06/13 13:32:49 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/06/14 10:55:28 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
 
+/*****************************************************************************/
+/*                                  INCLUDES                                 */
+/*****************************************************************************/
 #include <string>
 #include <iostream>
+#include <cctype>
+#include <exception>
+#include <vector>
+#include <list>
+#include <deque>
+#include <queue>
+#include <map>
+#include <iterator>
+#include <algorithm>
+
+/*****************************************************************************/
+/*                                  COLORS                                   */
+/*****************************************************************************/
+
+#define RED		"\033[0;31m"
+#define GREEN	"\033[0;32m"
+#define YELLOW	"\033[0;33m"
+#define ORANGE "\033[38;5;208m"
+#define BLUE	"\033[0;34m"
+#define MAGENTA	"\033[0;35m"
+#define CYAN	"\033[0;36m"
+#define WHITE	"\033[0;37m"
+#define BOLD       "\033[1m"
+#define UNDERLINE  "\033[4m"
+#define ITALIC     "\033[3m"
+#define RESET      "\033[0m"
+
+/*****************************************************************************/
+/*                                  TEMPLATES                                */
+/*****************************************************************************/
 
 template<typename T>
+typename T::iterator easyfind(T &container, int nb)
+{
+    return (std::find(container.begin(), container.end(), nb));
+}
 
 
-
-#ifndef
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,96 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 11:54:25 by lgernido          #+#    #+#             */
-/*   Updated: 2024/06/13 11:54:35 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/06/14 13:10:21 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+
+int main(void)
+{
+    {
+        /*TESTING VECTORS*/
+        
+        std::vector<int> tab(10); 
+        int value = 1; 
+        std::fill(tab.begin(), tab.end(), value);
+        
+        /*Value that will be found*/
+        std::vector<int>::iterator found = easyfind(tab, 1);
+        if (found == tab.end())
+            std::cout << RED BOLD "[+]Vector[+]   Error 404 Could not find" << RESET << std::endl;
+        else
+            std::cout << GREEN BOLD"[+]Vector[+]   Yeah " << *found << " appears for the first time at index " << found - tab.begin() << RESET << std::endl;
+        
+        /*Value that won't be found*/
+        if (easyfind(tab, 210) == tab.end())
+            std::cout << RED BOLD "[+]Vector[+]   Error 404 Could not find" << RESET << std::endl;
+    }
+    {
+        /*TESTING LISTS*/
+        
+        std::list<int> tab(10);
+        int value = 9;
+        std::fill(tab.begin(), tab.end(), value);
+
+        /*Value that will be found*/
+        std::list<int>::iterator found = easyfind(tab, 9);
+        if (found == tab.end())
+            std::cout << RED BOLD "[+]List[+]     Error 404 Could not find" << RESET << std::endl;
+        else
+        {
+            int index = std::distance(tab.begin(), found);
+            std::cout << GREEN BOLD "[+]List[+]     Yeah " << *found << " appears for the first time at index " << index << RESET << std::endl;
+        }
+        
+        /*Value that won't be found*/
+        if (easyfind(tab,17) == tab.end())
+            std::cout << RED BOLD "[+]List[+]     Error 404 Could not find" << RESET << std::endl;
+    }
+    {
+        /*TESTING DEQUE*/
+        
+        std::deque<int> tab(10);
+        int value = 9;
+        std::fill(tab.begin(), tab.end(), value);
+
+        /*Value that will be found*/
+        std::deque<int>::iterator found = easyfind(tab, 9);
+        if (found == tab.end())
+            std::cout << RED BOLD "[+]Deque[+]    Error 404 Could not find" << RESET << std::endl;
+        else
+        {
+            int index = std::distance(tab.begin(), found);
+            std::cout << GREEN BOLD "[+]Deque[+]    Yeah " << *found << " appears for the first time at index " << index << RESET << std::endl;
+        }
+        
+        /*Value that won't be found*/
+        if (easyfind(tab,17) == tab.end())
+            std::cout << RED BOLD "[+]Deque[+]    Error 404 Could not find" << RESET << std::endl;
+    }
+    {
+        /*TESTING QUEUE*/
+        
+        std::queue<int> tab;
+        int value = 10;
+        
+        for (int i = 0; i < 10; ++i) 
+        {
+            tab.push(value);
+        }
+        /*No iterator in queue container*/
+        /*Value that will be found*/
+        if (!easyfind(tab, 9)) 
+            std::cout << RED BOLD "[+]Queue[+]   Error 404 Could not find" << RESET << std::endl;
+        else 
+            std::cout << GREEN BOLD "[+]Queue[+]   Yeah 9 appears in the queue" RESET << std::endl;
+        /*Value that won't be found*/
+        if (!easyfind(tab, 17)) 
+            std::cout << RED BOLD "[+]Queue[+]   Error 404 Could not find" << RESET << std::endl;
+    }
+
+        return 0;
+    }
+        
+    
